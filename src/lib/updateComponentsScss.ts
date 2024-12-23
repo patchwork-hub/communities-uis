f8f049892be9d5cec6b6677b489b8f1da2b2f8b4
Chan Nyein Tun <29679269+channyeintun@users.noreply.github.com>
2024-12-23 23:26:24 +0700

    refactor

--- src/lib/updateComponentsScss.ts
+++ src/lib/updateComponentsScss.ts
@@ -22,73 +22,73 @@ export const updateComponentsScss = () => {
   const fileContent = readFileSync(targetFilePath, "utf8");
 
   const addNewScss = `
-    .community__section-headline {
-            button,
-            a {
-              &.active {
-                &::before {
-                  width: 100%;
-                }
-              }
-            }
-          }
-          .communitiesCardContainer {
-            display: grid;
-            grid-template-columns: repeat(2, 1fr);
-            gap: 13px;
-            padding: 13px;
-          
-            .card {
-              cursor: pointer;
-              overflow: hidden;
-              border-radius: 5px;
-              background: #000;
-              position: relative;
-          
-              &.full {
-                grid-column: span 2;
-                height: 160px;
-              }
-          
-              &.half {
-                grid-column: span 1;
-                height: 140px;
-              }
-            }
-            .card img {
-              height: 100%;
-              width: 100%;
-              opacity: 0.8;
-              object-fit: cover;
-            }
-            .shadow {
-              position: absolute;
-              bottom: 0;
-              left: 0;
-              width: 100%;
-              height: 50%;
-              background: linear-gradient(
-                to bottom,
-                rgba(0, 0, 0, 0) 0%,
-                rgba(0, 0, 0, 0.5) 100%
-              );
-              pointer-events: none;
-            }
-            .cardContent {
-              position: absolute;
-              bottom: 10px;
-              left: 10px;
-              right: 10px;
-              font-weight: 500;
-              display: flex;
-              align-items: center;
-              justify-content: space-between;
-            }
-            .rightArrow {
-              width: 20px !important;
-              height: 20px !important;
-            }
-          }
+.community__section-headline {
+  button,
+  a {
+    &.active {
+      &::before {
+        width: 100%;
+      }
+    }
+  }
+}
+.communitiesCardContainer {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 13px;
+  padding: 13px;
+
+  .card {
+    cursor: pointer;
+    overflow: hidden;
+    border-radius: 5px;
+    background: #000;
+    position: relative;
+
+    &.full {
+      grid-column: span 2;
+      height: 160px;
+    }
+
+    &.half {
+      grid-column: span 1;
+      height: 140px;
+    }
+  }
+  .card img {
+    height: 100%;
+    width: 100%;
+    opacity: 0.8;
+    object-fit: cover;
+  }
+  .shadow {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 50%;
+    background: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0.5) 100%
+    );
+    pointer-events: none;
+  }
+  .cardContent {
+    position: absolute;
+    bottom: 10px;
+    left: 10px;
+    right: 10px;
+    font-weight: 500;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .rightArrow {
+    width: 20px !important;
+    height: 20px !important;
+  }
+}
     `.trim();
   replaceContent(fileContent, `${fileContent}\n${addNewScss}`, targetFilePath);
 
